5167c380a36526ff73429e62147671b15b4987da
Bryan Gallo <bryangallo95@hotmail.com>
2024-07-14 23:58:53 -0500

    Instalando uuid para generar ids unicos

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react";
+import {v4 as uuidv4} from "uuid";
 import { activityReducer, initialState } from "./reducer/activity-reducer";
 import Form from "./components/Form";
 
