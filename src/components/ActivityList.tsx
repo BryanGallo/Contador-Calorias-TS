c816b034622a243a181e40caa5da65f12cc96f0c
BryanGallo <bryangallo95@hotmail.com>
2024-07-19 21:03:13 -0500

    Recorriendo Activities y mostrando informacion
    Falta nombres categorias

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -7,9 +7,27 @@ const ActivityList = ({ activities }: ActivityList) => {
     console.log(activities);
 
     return (
-        <h2 className="text-slate-600 font-bold text-center text-4xl">
-            Comidas y Actividades
-        </h2>
+        <>
+            <h2 className="text-slate-600 font-bold text-center text-4xl">
+                Comidas y Actividades
+            </h2>
+            {activities.map((activity) => (
+                <div
+                    key={activity.id}
+                    className="px-5 py-10 bg-white mt-5 flex justify-between"
+                >
+                    <div className=" space-y-2 relative">
+                        <p className="font-bold">{activity.category}</p>
+                        <p className="text-2xl pt-5 font-black">
+                            {activity.name}
+                        </p>
+                        <p className="text-4xl pt-5 font-black text-lime-500">
+                            {activity.calories} <span>Calorias</span>
+                        </p>
+                    </div>
+                </div>
+            ))}
+        </>
     );
 };
 
