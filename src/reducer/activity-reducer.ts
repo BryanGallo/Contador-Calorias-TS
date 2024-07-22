d1df76e3c9cfe313ac260c16051cdd7d7a57a0ff
BryanGallo <bryangallo95@hotmail.com>
2024-07-22 13:13:05 -0500

    Creando accion para seleccionar actividad a editar
    en activity-reducer

--- src/reducer/activity-reducer.ts
+++ src/reducer/activity-reducer.ts
@@ -1,15 +1,19 @@
 import type { Activity } from "../types";
 
-export type ActivityActions = {
-    type: "save-activity";
-    payload: { newActivity: Activity };
-};
+export type ActivityActions =
+    | {
+          type: "save-activity";
+          payload: { newActivity: Activity };
+      }
+    | { type: "set-activeId"; payload: { id: Activity["id"] } };
 
 type ActivityState = {
     activities: Activity[];
+    activeId: Activity["id"];
 };
 export const initialState: ActivityState = {
     activities: [],
+    activeId: "",
 };
 export const activityReducer = (
     state: ActivityState = initialState,
@@ -18,10 +22,17 @@ export const activityReducer = (
     if (action.type === "save-activity") {
         //este codigo maneja la logica para manejar el state
         console.log(action.payload.newActivity);
+        return {
+            ...state,
+            activities: [...state.activities, action.payload.newActivity],
+        };
+    }
+    if(action.type === 'set-activeId'){
         return{
             ...state,
-            activities: [...state.activities, action.payload.newActivity]
+            activeId: action.payload.id
         }
     }
-    return state
+
+    return state;
 };
