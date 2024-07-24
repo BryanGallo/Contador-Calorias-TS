eae6451fba62da6ddfb207029221335c5f8a5e4a
BryanGallo <bryangallo95@hotmail.com>
2024-07-24 14:27:47 -0500

    UseMemo para saber si existen o no actividades
    ActivityList

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -17,61 +17,73 @@ const ActivityList = ({ activities, dispatch }: ActivityList) => {
         [activities]
     );
 
+    const isEmptyActivities = useMemo(
+        () => activities.length === 0,
+        [activities]
+    );
+    console.log(isEmptyActivities);
+
     return (
         <>
             <h2 className=" font-bold text-center text-4xl">
                 Comidas y Actividades
             </h2>
-            {activities.map((activity) => (
-                <div
-                    key={activity.id}
-                    className="px-5 pb-5 pt-1 bg-white mt-5 rounded-lg flex justify-between items-center w-2/3 mx-auto"
-                >
-                    <div className="space-y-2 flex flex-col ">
-                        <p
-                            className={` self-start text-sm font-bold px-2 py-2 text-white uppercase text-center rounded-r-lg ${
-                                activity.category === 1
-                                    ? "bg-orange-500"
-                                    : "bg-lime-500"
-                            }`}
-                        >
-                            {categoryName(+activity.category)}
-                        </p>
-                        <p className="text-2xl font-black ml-5">
-                            {activity.name}
-                        </p>
-                        <p className="text-4xl font-black text-lime-500 ml-5">
-                            {activity.calories} <span>Calorias</span>
-                        </p>
+            {isEmptyActivities ? (
+                <p className="text-center mt-5 text-3xl text-yellow-600">
+                    No hay actividades
+                </p>
+            ) : (
+                activities.map((activity) => (
+                    <div
+                        key={activity.id}
+                        className="px-5 pb-5 pt-1 bg-white mt-5 rounded-lg flex justify-between items-center w-2/3 mx-auto"
+                    >
+                        <div className="space-y-2 flex flex-col ">
+                            <p
+                                className={` self-start text-sm font-bold px-2 py-2 text-white uppercase text-center rounded-r-lg ${
+                                    activity.category === 1
+                                        ? "bg-orange-500"
+                                        : "bg-lime-500"
+                                }`}
+                            >
+                                {categoryName(+activity.category)}
+                            </p>
+                            <p className="text-2xl font-black ml-5">
+                                {activity.name}
+                            </p>
+                            <p className="text-4xl font-black text-lime-500 ml-5">
+                                {activity.calories} <span>Calorias</span>
+                            </p>
+                        </div>
+                        <div className="flex gap-5 items-center ">
+                            <button
+                                onClick={() => {
+                                    dispatch({
+                                        type: "set-activeId",
+                                        payload: { id: activity.id },
+                                    });
+                                }}
+                            >
+                                <PencilSquareIcon className="h-8 w-8 text-blue-800" />
+                            </button>
+                            <button
+                                onClick={() => {
+                                    const validation = confirm(
+                                        "Seguro quieres eliminar esta actividad"
+                                    );
+                                    if (!validation) return;
+                                    dispatch({
+                                        type: "delete-activity",
+                                        payload: { id: activity.id },
+                                    });
+                                }}
+                            >
+                                <TrashIcon className="h-8 w-8 text-red-500" />
+                            </button>
+                        </div>
                     </div>
-                    <div className="flex gap-5 items-center ">
-                        <button
-                            onClick={() => {
-                                dispatch({
-                                    type: "set-activeId",
-                                    payload: { id: activity.id },
-                                });
-                            }}
-                        >
-                            <PencilSquareIcon className="h-8 w-8 text-blue-800" />
-                        </button>
-                        <button
-                            onClick={() => {
-                                const validation = confirm(
-                                    "Seguro quieres eliminar esta actividad"
-                                );
-                                if (!validation) return;
-                                dispatch({
-                                    type: "delete-activity",
-                                    payload: { id: activity.id },
-                                });
-                            }}
-                        >
-                            <TrashIcon className="h-8 w-8 text-red-500" />
-                        </button>
-                    </div>
-                </div>
-            ))}
+                ))
+            )}
         </>
     );
 };
