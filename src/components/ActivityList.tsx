2278f0e4c987f66c538ea737c2bfc265888c39a2
BryanGallo <bryangallo95@hotmail.com>
2024-07-24 14:08:01 -0500

    Boton para eliminar y con icon Trash
    ActivityList

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { useMemo, Dispatch } from "react";
 import { Activity } from "../types";
 import { categories } from "../data/categories";
-import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { PencilSquareIcon, TrashIcon } from "@heroicons/react/24/outline";
 import { ActivityActions } from "../reducer/activity-reducer";
 
 type ActivityList = {
@@ -53,7 +53,17 @@ const ActivityList = ({ activities, dispatch }: ActivityList) => {
                                 });
                             }}
                         >
-                            <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+                            <PencilSquareIcon className="h-8 w-8 text-blue-800" />
+                        </button>
+                        <button
+                            onClick={() => {
+                                dispatch({
+                                    type: "delete-activity",
+                                    payload: { id: activity.id },
+                                });
+                            }}
+                        >
+                            <TrashIcon className="h-8 w-8 text-red-500" />
                         </button>
                     </div>
                 </div>
